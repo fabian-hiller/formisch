c07df240d2f8ccfafd5e3de099a9cc4bf25a4b14
Fabian Hiller <hillerfabian11@gmail.com>
2025-04-04 23:46:02 -0400

    Setup repository

---
+++ prettier.config.js
@@ -0,0 +1,23 @@
+export default {
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  // prettier-ignore
+  // eslint-disable-next-line no-useless-escape
+  importOrder: ['^\w', '^[./]'],
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+};
